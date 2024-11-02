7f89e213eeeb16be494797533f5e8b2697814d4c
Marcos-Capristo <marcos.capristo@gmail.com>
2024-11-02 13:24:14 -0300

    ajustes vários

--- newreportapp/static/js/scripts.js
+++ newreportapp/static/js/scripts.js
@@ -1,5 +1,7 @@
 //newreportapp/static/js/scripts.js
 
+
+/*
 function startVoiceRecognition(inputId) {
     const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
     recognition.lang = 'pt-BR'; // Defina o idioma conforme necessário
@@ -16,4 +18,30 @@ function startVoiceRecognition(inputId) {
     recognition.onerror = function(event) {
         console.error('Error occurred in recognition: ' + event.error);
     };
-}
+
+    */
+    function hideMsg(selector, duration) {
+        document.addEventListener('DOMContentLoaded', function() {
+            setTimeout(function() {
+                var alerts = document.querySelectorAll(selector);
+                alerts.forEach(function(alert) {
+                    // Define a opacidade inicial
+                    let opacity = 1;
+                    // Define a duração do fade out
+                    const fadeOutDuration = 800;
+                    const interval = fadeOutDuration / 100; // Número de incrementos (100)
+    
+                    const fadeOut = setInterval(function() {
+                        if (opacity <= 0) {
+                            clearInterval(fadeOut);
+                            alert.style.display = 'none'; // Esconde o elemento após a animação
+                        } else {
+                            opacity -= 0.01; // Decrementa a opacidade
+                            alert.style.opacity = opacity; // Aplica a opacidade ao elemento
+                        }
+                    }, interval);
+                });
+            }, duration);
+        });
+    }
+
--- newreportapp/urls.py
+++ newreportapp/urls.py
@@ -1,37 +1,27 @@
-# newreportapp/urls.py
 from django.urls import path
 from django.contrib.auth import views as auth_views
-
-from .views import register_user, user_login_view, home_views, edit_user_profile_view, create_post_view, post_likes_views, post_delete_views, change_password_view
+from .views import change_password_view, edit_user_profile_view, register_user, post_delete_views, post_likes_views, user_login_view, home_views, create_post_view
 
 urlpatterns = [
     # URLs de redefinição de senha com templates personalizados
-    path(
-        'password_reset/', 
+    path('password_reset/', 
         auth_views.PasswordResetView.as_view(template_name='password_reset_form.html'), 
         name='password_reset'
     ),
-    path(
-        'password_reset/done/', 
+    path('password_reset/done/', 
         auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), 
         name='password_reset_done'
     ),
-    path(
-        'reset/<uidb64>/<token>/', 
+    path('reset/<uidb64>/<token>/', 
         auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), 
         name='password_reset_confirm'
     ),
-    path(
-        'reset/done/', 
+    path('reset/done/', 
         auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), 
         name='password_reset_complete'
     ),
-    path(
-        'password_change_custom/',
-        auth_views.PasswordChangeView.as_view(template_name='registration/password_change_custom.html'),
-        name='password_change'
-    ),
-    
+    #path('password_change/', change_password_view, name='password_change'),  # Usando a view personalizada
+    path('account/password_change/', change_password_view, name='password_change'),
     # URLs relacionadas à conta do usuário
     path('account/register/', register_user, name='register'),
     path('account/login/', user_login_view, name='login'),
@@ -42,7 +32,4 @@ urlpatterns = [
 
     path('account/edit_user_profile/', edit_user_profile_view, name='edit_user_profile'),
     path('create_post', create_post_view, name='create_post'),
-    
-    # Página inicial
-    # path('', index, name='index'),
 ]
