e5fdcf220161b05244ec4f4addae15ef2a4a7c81
Marcos-Capristo <marcos.capristo@gmail.com>
2024-11-05 12:03:44 -0300

    Estilizado msg de erro no base.html, implementado lógica para editar postagem somente se não houver comentários.

--- newreportapp/views/create_post_views.py
+++ newreportapp/views/create_post_views.py
@@ -1,13 +1,18 @@
-# newreportapp/views/create_post_views.py
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages  # Importa o sistema de mensagens
 from newreportapp.forms.post_form import PostForm
-from newreportapp.models import PostModel
+from newreportapp.models import PostModel  # Importando o modelo de postagens
+from newreportapp.models import ComentPostModel  # Importando o modelo de comentários
 from django.contrib.auth.decorators import login_required
-from newreportapp.models import LikePost
 
 @login_required
 def create_post_view(request, post_id=None):
     post = get_object_or_404(PostModel, id=post_id) if post_id else None
+
+    # Verifica se a postagem já possui comentários
+    if post and post.comments.exists():
+        messages.error(request, 'Mensagens comentadas não podem ser editadas, somente deletadas.')  # Adiciona a mensagem de erro
+        return redirect('home')  # Redireciona para a página inicial
 
     if request.method == 'POST':
         form = PostForm(request.POST, request.FILES, instance=post)
