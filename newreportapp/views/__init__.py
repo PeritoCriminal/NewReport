e85be0355fb9b7bda8a2ab1c32160a5a7805673f
Marcos-Capristo <marcos.capristo@gmail.com>
2025-03-01 01:41:23 -0300

    ajustes na ordenação de sections

--- newreportapp/views/__init__.py
+++ newreportapp/views/__init__.py
@@ -32,4 +32,5 @@ from .report.conclusion_views import conclusion_view
 from .report.considerations_views import considerations_view
 from .report.for_analysis_views import for_analysis_view
 from .report.for_custody_views import for_custody_view
+from .report.location_data_report_views import location_data_view
 # from .report.load_image_data_views import load_image_data
---
+++ newreportapp/views/report/location_data_report_views.py
@@ -0,0 +1,112 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from newreportapp.models import SectionReportModel, HeaderReportModel, ImageReportModel
+from newreportapp.forms import SectionReportForm
+from django.http import JsonResponse
+from django.contrib import messages
+
+
+def location_data_view(request, id=None, header_report_id=None):
+    subject = "location_data"
+
+    # Alterar o valor do atributo abaixo para diferentes subsecções.
+    title = "Dados de Localização"
+
+    # Alterar rota com o nome no arquivo URLS.
+    rota = "edit_location_data"
+
+    order = 10
+
+    existing_report = None
+    if header_report_id:
+        existing_report = SectionReportModel.objects.filter(
+            header_report_id=header_report_id,
+            rota=rota,
+        ).first()
+
+    # Decide entre abrir para edição ou criar novo registro.
+    if existing_report:
+        report_section = existing_report
+        title = report_section.title
+        action = "Edição"
+    else:
+        report_section = SectionReportModel()
+        action = "Criação"
+
+    # Obtem o `HeaderReportModel` correspondente, se aplicável.
+    header_report = get_object_or_404(HeaderReportModel, id=header_report_id) if header_report_id else None
+
+    # Obtém as imagens associadas ao report_section.
+    images = report_section.images.all() if existing_report else []
+
+    # Cria o formulário.
+    form = SectionReportForm(
+        request.POST or None,
+        instance=report_section,
+        initial={
+            'title': title,
+            'rota': rota,
+            'order': order,
+        }
+    )
+
+    # Atualiza o `placeholder` do campo `description`.
+    place_holder_for_description = """Campo opcional.
+    Utilize para indicar a localização, como endereço ou coordenadas geográficas."""
+    form.fields['description'].widget.attrs.update({'placeholder': place_holder_for_description})
+
+    if request.method == "POST":
+        # Atualiza os campos fixos
+        if header_report:
+            report_section.header_report = header_report
+            report_section.subject = subject
+            report_section.rota = rota
+            report_order = 10
+
+        # Atualiza manualmente os campos que podem não estar vindo corretamente no POST
+        post_data = request.POST.copy()
+        post_data["title"] = title  # Garante que o título seja enviado para o formulário
+        post_data["order"] = report_order
+        form = SectionReportForm(post_data, instance=report_section)
+
+        if form.is_valid():
+            section_report = form.save(commit=False)
+
+            # Pega a descrição do POST corretamente
+            section_report.description = request.POST.get("data_location_description", "")
+            section_report.order = report_order
+
+            section_report.save()
+
+            messages.success(request, f"{action} do relatório realizada com sucesso!")
+            return redirect('show_report', pk=header_report.id)
+        else:
+            print(f'Formulário inválido - Erros: {form.errors}')
+
+
+    return render(request, 'report/location_data_report.html', {
+        'report_section': report_section,
+        'report_section_id': report_section.id,
+        'form': form,
+        'action': action,
+        'header_report_id': header_report_id,
+        'subject': subject,
+        'title': title,
+        'rota': rota,
+        'images': images,
+        'place_holder_for_description': place_holder_for_description,
+    })
+
+
+def load_image_data(request):
+    """Carrega os dados de uma imagem para edição."""
+    if request.method == "GET" and 'image_id' in request.GET:
+        image_id = request.GET.get('image_id')
+        image = get_object_or_404(ImageReportModel, id=image_id)
+
+        data = {
+            'id': image.id,
+            'description': image.description,
+            'caption': image.caption,
+        }
+        return JsonResponse(data)
+    return JsonResponse({'error': 'Requisição inválida.'}, status=400)
